a1c3ef82db211df15caa97db1df4ee1358606d67
Lionnel7 <pierrerichan@gmail.com>
2025-08-22 18:05:59 +0200

    jjj

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,23 @@
+export default class Airport {
+    constructor(name, code) {
+      this._name = name;
+      this._code = code;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    get code() {
+      return this._code;
+    }
+  
+    get [Symbol.toStringTag]() {
+      return this._code;
+    }
+  
+    // Overrides the toString method to return the airport code
+    toString() {
+      return `[object ${this._code}]`;
+    }
+  }
